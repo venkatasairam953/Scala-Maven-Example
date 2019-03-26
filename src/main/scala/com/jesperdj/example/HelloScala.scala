b89a70bf3e9bb62a230a3e693a288845ef2d2cdf
sai <venkatasairam953@gmail.com>
2019-03-27 00:29:35 +0530

    added changes

--- src/main/scala/com/jesperdj/example/HelloScala.scala
+++ src/main/scala/com/jesperdj/example/HelloScala.scala
@@ -1,5 +1,8 @@
 package com.jesperdj.example
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
 
 object HelloScala extends App {
   println("Hello Scala")
+  println("Hello Scala Programm")
 }
